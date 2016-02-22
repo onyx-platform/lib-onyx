a33da1436b1cd75af852fcc13930915af4a9a318
Michael Drogalis <mjd3089@rit.edu>
2016-02-21 20:17:53 -0800

    Docstrings for each endpoint.

--- src/lib_onyx/replica_query_server.clj
+++ src/lib_onyx/replica_query_server.clj
@@ -12,71 +12,84 @@
   (UUID/fromString x))
 
 (def endpoints
-  {{:uri "/replica/peers"
+  {^{:doc "Lists all the peer idss."}
+   {:uri "/replica/peers"
     :request-method :get}
    (fn [request log-subscriber replica]
      (rq/peers replica))
 
+   ^{:doc "Lists all non-killed, non-completed job ids."}
    {:uri "/replica/jobs"
     :request-method :get}
    (fn [request log-subscriber replica]
      (rq/jobs replica))
 
+   ^{:doc "Lists all the job ids that have been killed."}
    {:uri "/replica/killed-jobs"
     :request-method :get}
    (fn [request log-subscriber replica]
      (rq/killed-jobs replica))
 
+   ^{:doc "Lists all the job ids that have been completed."}
    {:uri "/replica/completed-jobs"
     :request-method :get}
    (fn [request log-subscriber replica]
      (rq/completed-jobs replica))
 
+   ^{:doc "Given a job ids, returns all the task ids for this job."}
    {:uri "/replica/tasks"
     :request-method :get}
    (fn [request log-subscriber replica]
      (let [job-id (get-in request [:query-params "job-id"])]
        (rq/tasks replica (parse-uuid job-id))))
 
+   ^{:doc "Returns a map of job id -> task id -> peer ids, denoting which peers are assigned to which tasks."}
    {:uri "/replica/job-allocation"
     :request-method :get}
    (fn [request log-subscriber replica]
      (rq/job-allocations replica))
 
+   ^{:doc "Given a job id, returns a map of task id -> peer ids, denoting which peers are assigned to which tasks for this job only."}
    {:uri "/replica/task-allocation"
     :request-method :get}
    (fn [request log-subscriber replica]
      (rq/task-allocations replica))
 
+   ^{:doc "Given a peer id, returns the Aeron hostname and port that this peer advertises to the rest of the cluster."}
    {:uri "/replica/peer-site"
     :request-method :get}
    (fn [request log-subscriber replica]
      (let [peer-id (get-in request [:query-params "peer-id"])]
        (rq/peer-site replica (parse-uuid peer-id))))
 
+   ^{:doc "Given a peer id, returns its current execution state (e.g. :idle, :active, etc)."}
    {:uri "/replica/peer-state"
     :request-method :get}
    (fn [request log-subscriber replica]
      (let [peer-id (get-in request [:query-params "peer-id"])]
        (rq/peer-state replica (parse-uuid peer-id))))
 
+   ^{:doc "Given a peer id, returns the job id and task id that this peer is currently assigned to, if any."}
    {:uri "/replica/peer-allocation"
     :request-method :get}
    (fn [request log-subscriber replica]
      (let [peer-id (get-in request [:query-params "peer-id"])]
        (rq/peer-allocation replica (parse-uuid peer-id))))
 
+   ^{:doc "Returns the job scheduler for this tenancy of the cluster."}
    {:uri "/replica/job-scheduler"
     :request-method :get}
    (fn [request log-subscriber replica]
      (rq/job-scheduler replica))
 
+   ^{:doc "Given a job id, returns the task scheduler for this job."}
    {:uri "/replica/task-scheduler"
     :request-method :get}
    (fn [request log-subscriber replica]
      (let [job-id (get-in request [:query-params "job-id"])]
        (rq/task-scheduler replica (parse-uuid job-id))))
 
+   ^{:doc "Given a job id, returns workflow for this job."}
    {:uri "/job/workflow"
     :request-method :get}
    (fn [request log-subscriber replica]
@@ -84,6 +97,7 @@
                        (parse-uuid))]
        (jq/workflow log-subscriber job-id)))
 
+   ^{:doc "Given a job id, returns catalog for this job."}
    {:uri "/job/catalog"
     :request-method :get}
    (fn [request log-subscriber replica]
@@ -91,6 +105,7 @@
                        (parse-uuid))]
        (jq/catalog log-subscriber job-id)))
 
+   ^{:doc "Given a job id, returns flow conditions for this job."}
    {:uri "/job/flow-conditions"
     :request-method :get}
    (fn [request log-subscriber replica]
@@ -98,6 +113,7 @@
                        (parse-uuid))]
        (jq/flow-conditions log-subscriber job-id)))
 
+   ^{:doc "Given a job id, returns lifecycles for this job."}
    {:uri "/job/lifecycles"
     :request-method :get}
    (fn [request log-subscriber replica]
@@ -105,6 +121,7 @@
                        (parse-uuid))]
        (jq/lifecycles log-subscriber job-id)))
 
+   ^{:doc "Given a job id, returns windows for this job."}
    {:uri "/job/windows"
     :request-method :get}
    (fn [request log-subscriber replica]
@@ -112,6 +129,7 @@
                        (parse-uuid))]
        (jq/windows log-subscriber job-id)))
 
+   ^{:doc "Given a job id, returns triggers for this job."}
    {:uri "/job/triggers"
     :request-method :get}
    (fn [request log-subscriber replica]
@@ -119,6 +137,7 @@
                        (parse-uuid))]
        (jq/triggers log-subscriber job-id)))
 
+   ^{:doc "Given a job id and task id, returns catalog entry for this task."}
    {:uri "/job/task"
     :request-method :get}
    (fn [request log-subscriber replica]
