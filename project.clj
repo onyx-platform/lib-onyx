f124ac9f2e4be2d4dde2561d4b0cfe34a9112104
Lucas Bradstreet <lucasbradstreet@gmail.com>
2016-03-16 02:46:59 +0800

    Bump version after manual release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.onyxplatform/lib-onyx "0.8.12.0-SNAPSHOT"
+(defproject org.onyxplatform/lib-onyx "0.8.12.1-SNAPSHOT"
   :description "A library to support additional functionality in Onyx"
   :url "https://github.com/onyx-platform/lib-onyx"
   :license {:name "Eclipse Public License"
