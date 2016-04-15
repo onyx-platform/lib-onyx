79936695537b19fb3e00158129ee5da717c1d843
Gardner Vickers <gardner@vickers.me>
2016-04-14 22:29:28 -0700

    prepare for next release cycle

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.onyxplatform/lib-onyx "0.9.0.0"
+(defproject org.onyxplatform/lib-onyx "0.9.0.1-SNAPSHOT"
   :description "A library to support additional functionality in Onyx"
   :url "https://github.com/onyx-platform/lib-onyx"
   :license {:name "Eclipse Public License"
