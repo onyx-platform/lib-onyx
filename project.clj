fe527fae6239227ba269601f7a0ddc8a5ef93850
Lucas Bradstreet <lucasbradstreet@gmail.com>
2017-02-02 22:54:16 -0800

    Upgrade to 0.10.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.onyxplatform/lib-onyx "0.9.15.0"
+(defproject org.onyxplatform/lib-onyx "0.10.0.0-alpha1"
   :description "A library to support additional functionality in Onyx"
   :url "https://github.com/onyx-platform/lib-onyx"
   :license {:name "Eclipse Public License"
@@ -12,16 +12,15 @@
                              :password :env
                              :sign-releases false}}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [prismatic/schema "1.1.1"]
                  [org.clojure/tools.cli "0.3.5"]
                  [com.stuartsierra/component "0.3.1"]
                  [joplin.core "0.3.6"]
                  [cheshire "5.5.0"]
                  [ring-jetty-component "0.3.0"]
                  [ring "1.4.0"]
-                 [aero "1.0.0-beta2"]]
+                 [aero "1.0.3"]]
   :profiles {:dev {:dependencies [^{:voom {:repo "git@github.com:onyx-platform/onyx.git" :branch "master"}}
-                                  [org.onyxplatform/onyx "0.9.15"]
+                                  [org.onyxplatform/onyx "0.10.0-alpha1"]
                                   [de.ubercode.clostache/clostache "1.4.0"]]
                    :plugins [[codox "0.8.8"]
                              [lein-set-version "0.4.1"]
