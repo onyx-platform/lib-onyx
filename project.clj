c26ecbf98d0fe3201d71d7eba5b969a107ba423e
Lucas Bradstreet <lucasbradstreet@gmail.com>
2017-02-08 01:54:21 -0800

    Release 0.10.0.0 for use in onyx-template.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.onyxplatform/lib-onyx "0.10.0.0-alpha1"
+(defproject org.onyxplatform/lib-onyx "0.10.0.0"
   :description "A library to support additional functionality in Onyx"
   :url "https://github.com/onyx-platform/lib-onyx"
   :license {:name "Eclipse Public License"
