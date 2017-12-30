7c8caf248afd70501c78454d82bd39f62ba138f8
OnyxBot <michael.drogalis@onyxplatform.org>
2017-12-30 23:57:54 +0000

    Prepare for next release cycle.

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ lib-onyx is a utilities library to make working with Onyx's extensibility featur
 On Clojars:
 
 ```
-[org.onyxplatform/lib-onyx "0.12.2.0"]
+[org.onyxplatform/lib-onyx "0.12.2.1-SNAPSHOT"]
 ```
 
 ### API docs
--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.onyxplatform/lib-onyx "0.12.2.0"
+(defproject org.onyxplatform/lib-onyx "0.12.2.1-SNAPSHOT"
   :description "A library to support additional functionality in Onyx"
   :url "https://github.com/onyx-platform/lib-onyx"
   :license {:name "Eclipse Public License"
