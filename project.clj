f00ad28a84185257e461f28e63d7918a675d44bd
OnyxBot <michael.drogalis@onyxplatform.org>
2018-08-06 00:18:29 +0000

    Prepare for next release cycle.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.onyxplatform/lib-onyx "0.13.3.0-alpha4"
+(defproject org.onyxplatform/lib-onyx "0.13.3.0-SNAPSHOT"
   :description "A library to support additional functionality in Onyx"
   :url "https://github.com/onyx-platform/lib-onyx"
   :license {:name "Eclipse Public License"
